15ace900caf8211ff0be24e31c658eab0dacfd73
aminya <aminyahyaabadi74@gmail.com>
2020-07-17 12:57:48 -0500

    add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import { createPlugins } from "rollup-plugin-atomic";
+
+const plugins = createPlugins(["js", "coffee"], true);
+
+export default [
+  {
+    input: "lib/main.js",
+    output: [
+      {
+        dir: "dist",
+        format: "cjs",
+        sourcemap: true,
+      },
+    ],
+    // loaded externally
+    external: [
+      "atom",
+    ],
+    plugins: plugins,
+  },
+];
